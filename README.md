9dd04729d22dea9da458163fc74c3cd0e1ffa109
BlackPike <BlackPike@>
2023-08-08 17:45:29 +0300

    main

---
+++ README.md
@@ -0,0 +1,17 @@
+Running the forum:
+Make sure you have docker installed via:
+https://www.docker.com/get-started
+Download the docker extension for your VS code
+Insert the following to your VS code terminal:
+bash build.sh
+Opening the forum:
+Open the browser
+Type or copy+paste this to the searchbar:
+http://localhost:8080/
+Check if the audit questions are answered, you can find the questions at:
+https://git.01.alem.school/root/public/src/branch/master/subjects/forum/audit
+
+*List of Logins + passwords:
+BlackPike + 123
+Nerd + 123
+Sportman + 123
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	tpl            *template.Template
+	sqliteDatabase *sql.DB
+)
+
+func init() {
+	ReCreateAndConnSqlDataBase()
+	tpl = template.Must(template.ParseGlob("static/templates/*"))
+}
+
+func main() {
+	tpl, _ = template.ParseGlob("static/templates/*.html")
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.HandleFunc("/posts/", makeHandler(postsMiddleware))
+	http.HandleFunc("/categories/", makeHandler(categoryHandler))
+	http.HandleFunc("/signup", signupHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/logout", logoutHandler)
+	http.HandleFunc("/comments", commentsHandler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/reactpost", reactPostHandler)
+	http.HandleFunc("/reactcomment", reactCommentHandler)
+	http.HandleFunc("/showliked", showLikedHandler)
+
+	fmt.Printf("Starting server at port 8080\nlink: http://localhost:8080/\n")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
